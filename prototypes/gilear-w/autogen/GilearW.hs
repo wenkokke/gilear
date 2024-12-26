aa051e174ab7715c45b1c5683684617cc4104856
Wen Kokke <wenkokke@users.noreply.github.com>
2024-12-26 17:12:38 +0000

    chore: Remove autogen

--- prototypes/gilear-w/autogen/GilearW.hs
+++
@@ -1,9 +0,0 @@
-module GilearW where
-
-import GilearW.Internal.Ix (Ix (..))
-
-thin :: Ix -> Ix -> Ix
-thin FZ y = FS y
-thin (FS _) FZ = FZ
-thin (FS x) (FS y) = FS (thin x y)
-
--- prototypes/gilear-w/autogen/GilearW/Internal/Indexed.hs
+++
@@ -1,9 +0,0 @@
-module GilearW.Internal.Indexed where
-
-type All p = p
-
-infixr 0 :->
-type (:->) p q = p -> q
-
-newtype K p = K{unK :: p}
-
