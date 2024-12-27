1a97337fa41c6ff5ed613e5c2c168aaf7072e02f
Wen Kokke <wenkokke@users.noreply.github.com>
2024-12-27 17:00:17 +0000

    feat(gilear-w): Port Ty/Sch/Sub

--- prototypes/gilear-w/src/Gilear/W.hs
+++ prototypes/gilear-w/src/Gilear/W.hs
@@ -1,1 +1,29 @@
 module Gilear.W where
+
+import Control.Kripke (World)
+import Data.DeBruijn (Ix, Natural, S, Z)
+import Data.Kind (Type)
+import Data.Text (Text)
+
+data Name = Name
+  { text :: !Text
+  , uniq :: !Word
+  }
+
+data Ty (n :: Natural) :: Type where
+  E :: Name -> Ty n
+  U :: Ix n -> Ty n
+  (:->) :: Ty n -> Ty n -> Ty n
+
+newtype TyAt (w :: World) = TyAt (Ty Z)
+
+data Sch (n :: Natural) :: Type where
+  T :: Ty n -> Sch n
+  P :: Sch (S n) -> Sch n
+
+newtype SchAt (w :: World) = SchAt (Sch Z)
+
+data Sub (u :: World) (v :: World) = Sub
+  { target :: Name
+  , replacement :: Ty Z
+  }
