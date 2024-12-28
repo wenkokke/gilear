875f482ff4aabf261823dc20141f3a1790a9f863
Wen Kokke <wenkokke@users.noreply.github.com>
2024-12-28 17:41:38 +0000

    fix: partially fix definitions

--- prototypes/gilear-w/src/Gilear/W.hs
+++ prototypes/gilear-w/src/Gilear/W.hs
@@ -1,15 +1,20 @@
-{-# LANGUAGE LiberalTypeSynonyms, ExplicitNamespaces, RecordWildCards, GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LiberalTypeSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Gilear.W where
 
-import Control.Indexed (K(..), type (-->), type All)
-import Control.Kripke (World, Path(..), Kripke(..), MonadKripke(..), Box, box, Free(..))
-import Data.DeBruijn (Ix, Natural, S, Z, inject)
+import Control.Indexed (K (..), type All, type (-->))
+import Control.Kripke (Box, Free (..), Kripke (..), MonadKripke (..), Path (..), World, box)
+import Data.Bifunctor (bimap, first)
+import Data.DeBruijn (Ix (FS, FZ), Nat (S, Z), inject, thick, thin)
 import Data.Kind (Type)
-import Data.Text (Text)
+import Data.Proxy (Proxy (..))
 import Data.Set (Set)
+import Data.Text (Text)
 import Unsafe.Coerce (unsafeCoerce)
-import Data.Bifunctor (first, bimap)
 
 data Name = Name
   { text :: !Text
@@ -17,14 +22,14 @@ data Name = Name
   }
   deriving (Eq, Ord)
 
-data Ty (n :: Natural) :: Type where
+data Ty (n :: Nat) :: Type where
   ExVar :: Name -> Ty n
   UnVar :: Ix n -> Ty n
   (:->) :: Ty n -> Ty n -> Ty n
 
 newtype TyAt (w :: World) = TyAt (Ty Z)
 
-data Scheme (n :: Natural) :: Type where
+data Scheme (n :: Nat) :: Type where
   Mono :: Ty n -> Scheme n
   Poly :: Scheme (S n) -> Scheme n
 
@@ -35,15 +40,18 @@ data Subst (u :: World) (v :: World) = Subst
   , replacement :: Ty Z
   }
 
-instance Kripke Subst TyAt where
-  step (TyAt t) s = TyAt (subst s t)
+closed :: f Z -> f n
+closed = unsafeCoerce
 
 subst :: Subst u v -> Ty n -> Ty n
-subst Subst {..} = go
-  where
-    go (ExVar name) | target == name = unsafeCoerce replacement
-    go (s :-> t) = go s :-> go t
-    go t = t
+subst Subst{..} = go
+ where
+  go (ExVar name) | target == name = closed replacement
+  go (s :-> t) = go s :-> go t
+  go t = t
+
+instance Kripke Subst TyAt where
+  step (TyAt t) s = TyAt (subst s t)
 
 instance Kripke Subst SchemeAt where
   step (SchemeAt sch) s = SchemeAt (go sch)
@@ -52,58 +60,61 @@ instance Kripke Subst SchemeAt where
       go (Mono t) = Mono (subst s t)
       go (Poly t) = Poly (go t)
 
--- instantiation
-stan :: Scheme (S Z) -> All (TyAt --> SchemeAt)
-stan sch (TyAt t) = Scheme (sub FZ sch)
-  where
-    sub :: Ix (S n) -> Scheme (S n) -> Scheme n
-    sub j (Poly ty) = Poly (sub (FS j) ty)
-    sub j (Mono ty) = Mono (go ty)
-      where
-        go e@(ExVar _) = e
-        go (UnVar i)   = maybe (unsafeCoerce t) (Poly) $ thick j i
-        go (s :-> t)   = go s :-> go t
+-- -- instantiation
+-- stan :: Scheme (S Z) -> All (TyAt --> SchemeAt)
+-- stan sch (TyAt t) = SchemeAt (sub FZ sch)
+--   where
+--     sub :: Ix (S n) -> Scheme (S n) -> Scheme n
+--     sub j (Mono t') = Mono (go t')
+--       where
+--         go :: Ty (S n) -> Ty (S n)
+--         go e@(ExVar _) = e
+--         go (UnVar i)   = case thick j i of
+--           Nothing -> closed t
+--           Just i' -> UnVar i'
+--         go (t1 :-> t2) = go t1 :-> go t2
+--     sub j (Poly t') = Poly (sub (FS j) t')
 
 -- generalisation
-gen :: All (K Name --> SchemeAt --> SchemeAt)
-gen (K e) (Sch sch) =
-  case go FZ sch of
-    (sch, True)  -> Scheme (Poly sch)
-    (_  , False) -> Scheme sch
-  where
-    go :: Ix (S n) -> Scheme n -> (Scheme (S n), Bool)
-    go j (Mono ty) = first Mono (euTy e j ty)
-    go j (Poly ty) = first Poly (go (FS j) ty)
+-- gen :: All (K Name --> SchemeAt --> SchemeAt)
+-- gen (K e) (Sch sch) =
+--   case go FZ sch of
+--     (sch, True)  -> Scheme (Poly sch)
+--     (_  , False) -> Scheme sch
+--   where
+--     go :: Ix (S n) -> Scheme n -> (Scheme (S n), Bool)
+--     go j (Mono ty) = first Mono (euTy e j ty)
+--     go j (Poly ty) = first Poly (go (FS j) ty)
 
-    euTy :: Name -> Ix (S n) -> Ty n -> (Ty (S n), Bool)
-    euTy e j (ExVar x) =
-      if e == x then (UnVar j, True) else (ExVar x, False)
-    euTy _ j (UnVar i) = (UnVar (thin j i), False)
-    euTy e j (s :-> t) = bimap (:->) (||) (euTy e j s, euTy e j t)
+--     euTy :: Name -> Ix (S n) -> Ty n -> (Ty (S n), Bool)
+--     euTy e j (ExVar x) =
+--       if e == x then (UnVar j, True) else (ExVar x, False)
+--     euTy _ j (UnVar i) = (UnVar (thin j i), False)
+--     euTy e j (s :-> t) = bimap (:->) (||) (euTy e j s, euTy e j t)
 
 ---------------
 -- Effects
 ---------------
 
 data W (p :: World -> Type) (u :: World) where
-  Next        :: W (K Word) u
-  VarScheme   :: Name -> W SchemeAt u
+  Next :: W (K Word) u
+  VarScheme :: Name -> W SchemeAt u
   Instantiate :: SchemeAt u -> W TyAt u
-  Define      :: Set Name -> TyAt u -> Name -> W (K ()) u
-  BarfAllOver :: W f u  -- HardBarf for now
+  Define :: Set Name -> TyAt u -> Name -> W (K ()) u
+  BarfAllOver :: W f u -- HardBarf for now
 
-instance Kripke Subst (W p) where
-  step Next _ = Next
-  step (VarScheme x) _ = VarScheme x
-  step (Instantiate s) w = Instantiate (step s w)
-  step (Define setOfDependencies tyAtW x) w =
-    Define setOfDependencies (step tyAtW w) x
-  step BarfAllOver _ = BarfAllOver
+-- instance Kripke Subst (W p) where
+--   step Next _ = Next
+--   step (VarScheme x) _ = VarScheme x
+--   step (Instantiate s) w = Instantiate (step s w)
+--   step (Define setOfDependencies tyAtW x) w =
+--     Define setOfDependencies (step tyAtW w) x
+--   step BarfAllOver _ = BarfAllOver
 
-type FreeW p u = Free Subst W p u
-type SubKripke p = Kripke Subst p
+-- type FreeW p u = Free Subst W p u
+-- type SubKripke p = Kripke Subst p
 
-supply :: SubKripke p => Word -> All (FreeW p --> FreeW p)
-supply n (Call Next k) = supply (n + 1) $ k Refl (K n)
-supply _ p@(Pure _) = p
-supply n (Call c k) = Call c $ \ uv q -> supply n $ k uv q
+-- supply :: SubKripke p => Word -> All (FreeW p --> FreeW p)
+-- supply n (Call Next k) = supply (n + 1) $ k Refl (K n)
+-- supply _ p@(Pure _) = p
+-- supply n (Call c k) = Call c $ \ uv q -> supply n $ k uv q
