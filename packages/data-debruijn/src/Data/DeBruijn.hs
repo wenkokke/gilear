bf1767c945414e28621458f60ddff4135415cb3a
Wen Kokke <wenkokke@users.noreply.github.com>
2024-12-28 17:18:17 +0000

    fix: Lie To GHC, Convincingly

--- packages/data-debruijn/src/Data/DeBruijn.hs
+++ packages/data-debruijn/src/Data/DeBruijn.hs
@@ -1,54 +1,124 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RoleAnnotations #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
-module Data.DeBruijn where
+module Data.DeBruijn (
+  -- * Type-level natural numbers
+  Nat (..),
+  type Pred,
+  type (+),
 
-import Data.Kind (Type)
+  -- * De Bruijn Indices
+  Ix (FZ, FS),
+  toWord,
+  thin,
+  thick,
+  inject,
+) where
 
--- | The type of natural numbers.
-data N
-  = Z
-  | S N
+import Data.Kind (Constraint, Type)
+import Data.Proxy (Proxy)
+import Unsafe.Coerce (unsafeCoerce)
 
--- | The type of de Bruijn indices.
-data Ix (n :: N) :: Type where
-  FZ :: Ix (S n)
-  FS :: Ix n -> Ix (S n)
+-- | Type-level natural numbers.
+data Nat = Z | S Nat
 
-deriving stock instance (Eq (Ix n))
+-- | Addition of type-level naturals.
+type family (+) (n :: Nat) (m :: Nat) = (r :: Nat) where
+  Z + m = m
+  S n + m = S (n + m)
 
-deriving stock instance (Ord (Ix n))
+-- | Predecessor of type-level naturals.
+type family Pred (n :: Nat) = (r :: Nat) where
+  Pred (S n) = n
 
--- | The type of evidence that @n@ is positive.
-data Pos (n :: N) :: Type where
-  Pos :: Pos (S n)
+{-| @'SNat'@ is the singleton type for natural numbers.
+newtype SNat (n :: Nat) = UnsafeSN {unSN :: Word}
+type role SNat nominal
+-}
 
-deriving stock instance (Eq (Pos n))
+-- | @'Ix' n@ is the type of natural numbers less than @n@.
+newtype Ix (n :: Nat) = UnsafeIx {unIx :: Word}
 
-deriving stock instance (Ord (Pos n))
+type role Ix nominal
 
--- | The bound of any de Bruijn index is positive.
-isPos :: Ix n -> Pos n
-isPos FZ = Pos
-isPos (FS _) = Pos
-
--- | Convert the de Bruijn index to a 'Word'.
+-- | Convert an 'Ix' to 'Word'.
 toWord :: Ix n -> Word
-toWord FZ = 0
-toWord (FS i) = 1 + toWord i
+toWord = unIx
+
+-- | @'IxF'@ is the base functor of @'Ix'@.
+data IxF (ix :: Nat -> Type) (n :: Nat) :: Type where
+  FZF :: (S m ~ n) => IxF ix n
+  FSF :: (S m ~ n) => ix m -> IxF ix n
+
+-- | @'Dict' c@ is the type that holds the evidence for constraint @c@.
+data Dict (c :: Constraint) :: Type where
+  Dict :: (c) => Dict c
+
+unsafeHasPred :: forall n. Dict (S (Pred n) ~ n)
+unsafeHasPred = unsafeCoerce (Dict @(n ~ n))
+
+unsafeWithHasPred :: forall n f. f (S (Pred n)) -> f n
+unsafeWithHasPred i = case unsafeHasPred @n of Dict -> i
+
+suc :: Ix (Pred n) -> Ix n
+suc (UnsafeIx index) = UnsafeIx (index + 1)
+
+project :: Ix n -> IxF Ix n
+project (UnsafeIx index) =
+  unsafeWithHasPred $
+    if index == 0
+      then FZF
+      else FSF (UnsafeIx (index - 1))
+
+embed :: IxF Ix (S (Pred n)) -> Ix n
+embed FZF = UnsafeIx 0
+embed (FSF i) = suc i
+
+pattern FZ :: forall n. Ix n
+pattern FZ <- (project -> FZF)
+  where
+    FZ = embed FZF
+{-# INLINE FZ #-}
+
+pattern FS :: forall n. Ix (Pred n) -> Ix n
+pattern FS i <- (project -> FSF i)
+  where
+    FS i = embed (FSF i)
+{-# INLINE FS #-}
+
+{-# COMPLETE FZ, FS #-}
 
 -- | Thinning.
 thin :: Ix (S n) -> Ix n -> Ix (S n)
-thin FZ j = FS j
-thin (FS _) FZ = FZ
-thin (FS i) (FS j) = FS (thin i j)
+thin = thin'
+ where
+  thin' :: Ix n -> Ix (Pred n) -> Ix n
+  thin' FZ j = FS j
+  thin' (FS _) FZ = FZ
+  thin' (FS i) (FS j) = FS (thin' i j)
 
 -- | Thickening.
 thick :: Ix (S n) -> Ix (S n) -> Maybe (Ix n)
-thick FZ FZ = Nothing
-thick FZ (FS j) = Just j
-thick (FS i) FZ = case isPos i of Pos -> Just FZ
-thick (FS i) (FS j) = case isPos i of Pos -> FS <$> thick i j
+thick = thick'
+ where
+  thick' :: Ix n -> Ix n -> Maybe (Ix (Pred n))
+  thick' FZ FZ = Nothing
+  thick' FZ (FS j) = Just j
+  thick' (FS _) FZ = Just FZ
+  thick' (FS i) (FS j) = FS <$> thick' i j
+
+-- | Inject.
+inject :: Proxy n -> Ix m -> Ix (n + m)
+inject _ = unsafeCoerce
+
+{-| Raise.
+raise :: SNat n -> Ix m -> Ix (n + m)
+raise  SZ    i = i
+raise (SS n) i = FS (raise n i)
+-}
