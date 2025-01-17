71f4f5ef9ad52e490750ad5f805f200ad0224313
Wen Kokke <wenkokke@users.noreply.github.com>
2025-01-17 19:41:11 +0000

    fix(gilear-lsp): specialise MonadTc to LspTc

--- packages/gilear-lsp/src/Gilear/LSP/Internal/Handlers.hs
+++ packages/gilear-lsp/src/Gilear/LSP/Internal/Handlers.hs
@@ -17,24 +17,24 @@ import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as T
 import Data.Text.Lazy qualified as TL
-import Gilear.Internal.Core qualified as TC
+import Gilear.Internal.Core qualified as Tc
 import Gilear.Internal.Parser (InputEncoding (..))
-import Gilear.Internal.Parser qualified as TC
-import Gilear.Internal.Parser.Cache qualified as TC
+import Gilear.Internal.Parser qualified as Tc
+import Gilear.Internal.Parser.Cache qualified as Tc
 import Gilear.Internal.Parser.TextEdit (TextEdit (..))
 import Gilear.LSP.Internal.Compat.Gilear qualified as Gilear
-import Gilear.LSP.Internal.Core (LSPTC)
+import Gilear.LSP.Internal.Core (LspTc)
 import Language.LSP.Protocol.Lens (HasContentChanges (..), HasEnd (..), HasParams (..), HasRange (..), HasStart (..), HasText (..), HasTextDocument (..), HasUri (..))
 import Language.LSP.Protocol.Message (SMethod (..))
 import Language.LSP.Protocol.Types (ClientCapabilities, TextDocumentContentChangeEvent (..), TextDocumentContentChangePartial, toNormalizedUri, type (|?) (..))
-import Language.LSP.Server qualified as LSP
+import Language.LSP.Server qualified as Lsp
 import Language.LSP.VFS (file_text, file_version)
 import TreeSitter qualified as TS
 
 handlers ::
-  LogAction LSPTC (WithSeverity Text) ->
+  LogAction LspTc (WithSeverity Text) ->
   ClientCapabilities ->
-  LSP.Handlers LSPTC
+  Lsp.Handlers LspTc
 handlers logger clientCapabilities =
   mconcat
     [ initializedHandler
@@ -45,49 +45,49 @@ handlers logger clientCapabilities =
     , workspaceDidChangeConfiguration
     ]
  where
-  initializedHandler :: LSP.Handlers LSPTC
+  initializedHandler :: Lsp.Handlers LspTc
   initializedHandler =
-    LSP.notificationHandler SMethod_Initialized $ \_notification -> do
+    Lsp.notificationHandler SMethod_Initialized $ \_notification -> do
       logger <& WithSeverity (T.pack "ClientCapabilities: " <> TL.toStrict (encodeToLazyText clientCapabilities)) Debug
-      config <- LSP.getConfig
+      config <- Lsp.getConfig
       logger <& WithSeverity (T.pack "Config: " <> TL.toStrict (encodeToLazyText config)) Debug
       pure ()
 
-  textDocumentDidOpenHandler :: LSP.Handlers LSPTC
+  textDocumentDidOpenHandler :: Lsp.Handlers LspTc
   textDocumentDidOpenHandler =
-    LSP.notificationHandler SMethod_TextDocumentDidOpen $ \notification -> do
+    Lsp.notificationHandler SMethod_TextDocumentDidOpen $ \notification -> do
       let docUri = toNormalizedUri $ notification ^. params . textDocument . uri
       -- Try and get the virtual file for the uri:
-      LSP.getVirtualFile docUri >>= \case
+      Lsp.getVirtualFile docUri >>= \case
         -- If the virtual file does not exist...
         Nothing -> pure () -- TODO: report error
         Just docVirtualFile -> do
           let docRope = docVirtualFile ^. file_text
-          success <- TC.documentOpen logger InputEncodingUTF8 docUri docRope
+          success <- Tc.documentOpen logger InputEncodingUTF8 docUri docRope
           when success $ do
             let docVersion = docVirtualFile ^. file_version
             logger <& T.pack ("Successfully opened " <> show docUri <> " version " <> show docVersion) `WithSeverity` Debug
             Gilear.publishParserDiagnostics logger docUri (Just 0)
 
-  textDocumentDidSaveHandler :: LSP.Handlers LSPTC
+  textDocumentDidSaveHandler :: Lsp.Handlers LspTc
   textDocumentDidSaveHandler =
-    LSP.notificationHandler SMethod_TextDocumentDidSave $ \notification -> do
+    Lsp.notificationHandler SMethod_TextDocumentDidSave $ \notification -> do
       let _uri = notification ^. params . textDocument . uri
       pure ()
 
-  -- textDocumentSemanticTokensFullHandler :: LSP.Handlers LSPTC
+  -- textDocumentSemanticTokensFullHandler :: Lsp.Handlers LspTc
   -- textDocumentSemanticTokensFullHandler =
-  --   LSP.requestHandler SMethod_TextDocumentSemanticTokensFull $ \request responder -> do
+  --   Lsp.requestHandler SMethod_TextDocumentSemanticTokensFull $ \request responder -> do
   --     let _uri = request ^. params . textDocument . uri
   --     responder . Right . InR $ Null
 
-  textDocumentDidChangeHandler :: LSP.Handlers LSPTC
+  textDocumentDidChangeHandler :: Lsp.Handlers LspTc
   textDocumentDidChangeHandler =
-    LSP.notificationHandler SMethod_TextDocumentDidChange $ \notification -> do
+    Lsp.notificationHandler SMethod_TextDocumentDidChange $ \notification -> do
       let docUri = toNormalizedUri $ notification ^. params . textDocument . uri
       let docChanges = notification ^. params . contentChanges
       -- Try and get the virtual file for the uri:
-      LSP.getVirtualFile docUri >>= \case
+      Lsp.getVirtualFile docUri >>= \case
         -- If the virtual file does not exist...
         Nothing -> pure () -- TODO: report error
         Just docVirtualFile -> do
@@ -98,29 +98,29 @@ handlers logger clientCapabilities =
           success <-
             if docChangesWholeDocument
               -- ... try and parse the whole document from the VFS
-              then TC.documentChangeWholeDocument logger InputEncodingUTF8 docUri docRope
+              then Tc.documentChangeWholeDocument logger InputEncodingUTF8 docUri docRope
               -- Otherwise, parse the document incrementally...
-              else TC.documentChangePartial logger InputEncodingUTF8 docUri docRope docTextEdits
+              else Tc.documentChangePartial logger InputEncodingUTF8 docUri docRope docTextEdits
           when success $ do
             let docVersion = docVirtualFile ^. file_version
             logger <& T.pack ("Successfully changed " <> show docUri <> " version " <> show docVersion) `WithSeverity` Debug
             Gilear.publishParserDiagnostics logger docUri (Just . fromIntegral $ docVersion)
-            TC.lookupCache docUri >>= \case
+            Tc.lookupCache docUri >>= \case
               Nothing -> pure ()
               Just cacheItem -> do
-                rootNode <- liftIO (TS.treeRootNode (TC.itemTree cacheItem))
+                rootNode <- liftIO (TS.treeRootNode (Tc.itemTree cacheItem))
                 nodeText <- liftIO (T.decodeUtf8 <$> TS.showNode rootNode)
                 logger <& nodeText `WithSeverity` Debug
 
-  textDocumentDidCloseHandler :: LSP.Handlers LSPTC
+  textDocumentDidCloseHandler :: Lsp.Handlers LspTc
   textDocumentDidCloseHandler =
-    LSP.notificationHandler SMethod_TextDocumentDidClose $ \notification -> do
+    Lsp.notificationHandler SMethod_TextDocumentDidClose $ \notification -> do
       let _uri = notification ^. params . textDocument . uri
       pure ()
 
-  workspaceDidChangeConfiguration :: LSP.Handlers LSPTC
+  workspaceDidChangeConfiguration :: Lsp.Handlers LspTc
   workspaceDidChangeConfiguration =
-    LSP.notificationHandler SMethod_WorkspaceDidChangeConfiguration $ \_notification ->
+    Lsp.notificationHandler SMethod_WorkspaceDidChangeConfiguration $ \_notification ->
       pure ()
 
 changePartialToTextEdit :: TextDocumentContentChangePartial -> TextEdit
