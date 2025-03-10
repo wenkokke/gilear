f84f864959ea64be39fe0dd9e4e262a1173b5cea
Wen Kokke <wenkokke@users.noreply.github.com>
2025-03-10 14:03:52 +0000

    fix: repair type constraints on pattern synonyms and simplify definitions (#64)

--- packages/data-debruijn/src/Data/DeBruijn.hs
+++ packages/data-debruijn/src/Data/DeBruijn.hs
@@ -9,21 +9,19 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Data.DeBruijn (
-  -- * Type-level natural numbers
-  Nat (..),
-  type Pred,
+  type Nat (..),
   type (+),
-
-  -- * De Bruijn Indices
+  type Pred,
+  type Pos,
   Ix (FZ, FS),
   toWord,
+  isPos,
   thin,
   thick,
-  inject,
 ) where
 
 import Data.Kind (Constraint, Type)
-import Data.Proxy (Proxy)
+import Data.Word (Word16)
 import Unsafe.Coerce (unsafeCoerce)
 
 -- | Type-level natural numbers.
@@ -38,6 +36,10 @@ type family (+) (n :: Nat) (m :: Nat) = (r :: Nat) where
 type family Pred (n :: Nat) = (r :: Nat) where
   Pred (S n) = n
 
+-- | @'Pos' n@ holds if @n@ is non-zero.
+type Pos :: Nat -> Constraint
+type Pos (n :: Nat) = n ~ S (Pred n)
+
 {-| @'SNat'@ is the singleton type for natural numbers.
 newtype SNat (n :: Nat) = UnsafeSN {unSN :: Word}
 type role SNat nominal
@@ -50,89 +52,83 @@ data SNatF (snat :: Nat -> Type) (n :: Nat) :: Type where
 -}
 
 -- | @'Ix' n@ is the type of natural numbers less than @n@.
-newtype Ix (n :: Nat) = UnsafeIx {unIx :: Word}
-  deriving stock (Eq, Ord)
-  deriving newtype (Show)
+newtype Ix (n :: Nat) = UnsafeIx Word16
+
+deriving instance Eq (Ix n)
+
+deriving instance Ord (Ix n)
+
+instance Show (Ix n) where
+  show :: Ix n -> String
+  show (UnsafeIx u) = show u
 
 type role Ix nominal
 
 -- | Convert an 'Ix' to 'Word'.
-toWord :: Ix n -> Word
-toWord = unIx
+toWord :: Ix n -> Word16
+toWord (UnsafeIx u) = u
+{-# INLINE toWord #-}
 
 -- | @'IxF'@ is the base functor of @'Ix'@.
 data IxF (ix :: Nat -> Type) (n :: Nat) :: Type where
-  FZF :: (n ~ S m) => IxF ix n
-  FSF :: (n ~ S m) => !(ix m) -> IxF ix n
+  FZF :: IxF ix (S m)
+  FSF :: !(ix m) -> IxF ix (S m)
 
--- | @'Dict' c@ is the type that holds the evidence for constraint @c@.
-data Dict (c :: Constraint) :: Type where
-  Dict :: (c) => Dict c
-
-unsafeHasPred :: forall n. Dict (n ~ S (Pred n))
-unsafeHasPred = unsafeCoerce (Dict @(n ~ n))
-
-unsafeWithHasPred :: forall n f. f (S (Pred n)) -> f n
-unsafeWithHasPred i = case unsafeHasPred @n of Dict -> i
-
-unsafeSucIx :: Ix (Pred n) -> Ix n
-unsafeSucIx (UnsafeIx index) = UnsafeIx (index + 1)
+sucIx :: Ix n -> Ix (S n)
+sucIx (UnsafeIx u) = UnsafeIx (u + 1)
+{-# INLINE sucIx #-}
 
 projectIx :: Ix n -> IxF Ix n
-projectIx (UnsafeIx index) =
-  unsafeWithHasPred $
-    if index == 0
+projectIx (UnsafeIx u) =
+  unsafeCoerce $
+    if u == 0
       then FZF
-      else FSF (UnsafeIx (index - 1))
+      else FSF (UnsafeIx (u - 1))
+{-# INLINE projectIx #-}
 
-embedIx :: IxF Ix (S (Pred n)) -> Ix n
+embedIx :: IxF Ix (S n) -> Ix (S n)
 embedIx FZF = UnsafeIx 0
-embedIx (FSF i) = unsafeSucIx i
+embedIx (FSF i) = sucIx i
+{-# INLINE embedIx #-}
 
--- TODO:
--- Type signatures for pattern synonyms are weird.
--- We may be able to use that to simplify this code?
--- https://ghc.gitlab.haskell.org/ghc/doc/users_guide/exts/pattern_synonyms.html#typing-of-pattern-synonyms
+-- NOTE:
+--   Type signatures for pattern synonyms are weird, see:
+--   https://ghc.gitlab.haskell.org/ghc/doc/users_guide/exts/pattern_synonyms.html#typing-of-pattern-synonyms
 
-pattern FZ :: forall n. Ix n
-pattern FZ <- (projectIx -> FZF)
-  where
-    FZ = embedIx FZF
+pattern FZ :: () => (Pos n) => Ix n
+pattern FZ <- (projectIx -> FZF) where FZ = embedIx FZF
 {-# INLINE FZ #-}
 
-pattern FS :: forall n. Ix (Pred n) -> Ix n
-pattern FS i <- (projectIx -> FSF i)
-  where
-    FS i = embedIx (FSF i)
+pattern FS :: () => (Pos n) => Ix (Pred n) -> Ix n
+pattern FS i <- (projectIx -> FSF i) where FS i = embedIx (FSF i)
 {-# INLINE FS #-}
 
 {-# COMPLETE FZ, FS #-}
 
+-- | If any value of type @'Ix' n@ exists, @n@ must have a predecessor.
+isPos :: Ix n -> ((Pos n) => a) -> a
+isPos FZ r = r
+isPos (FS _) r = r
+
 -- | Thinning.
 thin :: Ix (S n) -> Ix n -> Ix (S n)
-thin = thin'
- where
-  thin' :: Ix n -> Ix (Pred n) -> Ix n
-  thin' FZ j = FS j
-  thin' (FS _) FZ = FZ
-  thin' (FS i) (FS j) = FS (thin' i j)
+thin FZ j = FS j
+thin (FS _) FZ = FZ
+thin (FS i) (FS j) = FS (thin i j)
 
 -- | Thickening.
 thick :: Ix (S n) -> Ix (S n) -> Maybe (Ix n)
-thick = thick'
- where
-  thick' :: Ix n -> Ix n -> Maybe (Ix (Pred n))
-  thick' FZ FZ = Nothing
-  thick' FZ (FS j) = Just j
-  thick' (FS _) FZ = Just FZ
-  thick' (FS i) (FS j) = FS <$> thick' i j
+thick FZ FZ = Nothing
+thick FZ (FS j) = Just j
+thick (FS i) FZ = isPos i $ Just FZ
+thick (FS i) (FS j) = isPos i $ FS <$> thick i j
 
--- | Inject.
-inject :: Proxy n -> Ix m -> Ix (n + m)
-inject _ = unsafeCoerce
+-- -- | Inject.
+-- inject :: Proxy n -> Ix m -> Ix (n + m)
+-- inject _ = unsafeCoerce
 
-{-| Raise.
-raise :: SNat n -> Ix m -> Ix (n + m)
-raise  SZ    i = i
-raise (SS n) i = FS (raise n i)
--}
+-- {-| Raise.
+-- raise :: SNat n -> Ix m -> Ix (n + m)
+-- raise  SZ    i = i
+-- raise (SS n) i = FS (raise n i)
+-- -}
